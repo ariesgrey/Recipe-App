d8a54531416559751b7733582c52fa7ec90d7f14
aries grey <ariesgrey88@gmail.com>
2023-07-30 22:50:34 -0500

    Updates

---
+++ precache-manifest.8e11cdd645361b4a457f44a82e205cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f38d7895b4758f93d15e9793c43440",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "ac1ff708d9de4e42731c",
+    "url": "/Recipe-App/static/css/main.f757606f.chunk.css"
+  },
+  {
+    "revision": "bc92c0fe2fde498abda7",
+    "url": "/Recipe-App/static/js/2.3de6cdc6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Recipe-App/static/js/2.3de6cdc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1ff708d9de4e42731c",
+    "url": "/Recipe-App/static/js/main.8406b1f9.chunk.js"
+  },
+  {
+    "revision": "203e95850772ca76296a",
+    "url": "/Recipe-App/static/js/runtime-main.461e0d4e.js"
+  }
+]);
